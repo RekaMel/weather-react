9772a13e085362dd046d0d2bc64979d16819aa0d
RekaMel <63839716+RekaMel@users.noreply.github.com>
2023-10-20 15:04:05 +0200

    added github link

--- src/App.js
+++ src/App.js
@@ -6,8 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World</p>
+        <h1>Weather App</h1>
         <Weather city="Paris" />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://funny-clafoutis-782363.netlify.app/" target="_blank">
+            Reka Acs
+          </a>{" "}
+          and is {""}
+          <a href="https://github.com/RekaMel/weather-react" target="_blank">
+            open sourced on GitHub.
+          </a>
+        </footer>
       </header>
     </div>
   );
