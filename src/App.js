ecef0813d0b0519d2a0c4cc1c92960a642fa6601
RekaMel <63839716+RekaMel@users.noreply.github.com>
2023-10-20 15:23:07 +0200

    Added npm for Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <Weather city="Paris" />
-        <footer>
-          This project was coded by{" "}
-          <a href="https://funny-clafoutis-782363.netlify.app/" target="_blank">
-            Reka Acs
-          </a>{" "}
-          and is {""}
-          <a href="https://github.com/RekaMel/weather-react" target="_blank">
-            open sourced on GitHub.
-          </a>
-        </footer>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+          <Weather city="Paris" />
+          <footer>
+            This project was coded by{" "}
+            <a
+              href="https://funny-clafoutis-782363.netlify.app/"
+              target="_blank"
+            >
+              Reka Acs
+            </a>{" "}
+            and is {""}
+            <a href="https://github.com/RekaMel/weather-react" target="_blank">
+              open sourced on GitHub.
+            </a>
+          </footer>
+        </header>
+      </div>
     </div>
   );
 }
